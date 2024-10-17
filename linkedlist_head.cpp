87e45de55999f74bcb0068954e182ad5ce7e544d
sunidhi <sunidhipriya0143@gmail.com>
2024-10-17 12:33:06 +0530

    second commit

--- linkedlist_head.cpp
+++ linkedlist_head.cpp
@@ -5,22 +5,22 @@ struct Node{
     int data;
     Node* next;
     public:
-    Node(int data1,Node* next1){
+    Node(int data1,Node* next1){ //construtor
         data=data1;
         next=next1;
     }
     public:
-    Node(int data1){
+    Node(int data1){       
         data=data1;
         next=nullptr;
     }
 };
 Node * convertArrtoLL(vector<int> & arr){
-    Node* head=new Node(arr[0]);
-    Node* mover=head;
+    Node* head=new Node(arr[0]); //head points to fist element
+    Node* mover=head; // initilly it points to head
     for(int i=1;i<arr.size();i++){
-        Node* temp=new Node(arr[i]);
-        mover->next=temp;
+        Node* temp=new Node(arr[i]);    
+        mover->next=temp;     //it mover forward
         mover=temp; // mover=mover->next
     }
     return head;
@@ -28,6 +28,6 @@ Node * convertArrtoLL(vector<int> & arr){
 int main(){
  vector<int> arr={12,5,4,67,64};
  Node* head=convertArrtoLL(arr);
- cout<<head->data;
+ cout<<head->data;  //print the data of the head
 return 0;
 }
